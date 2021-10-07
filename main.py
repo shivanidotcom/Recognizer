af9a6c7c32d79c613170265ef1191e7d40e13095
Shivani Vemula <76956616+shivanidotcom@users.noreply.github.com>
2021-10-07 20:59:52 +0530

    Add files via upload

--- main.py
+++ main.py
@@ -1,16 +1,6 @@
+
 import pyttsx3
-import speech_recognition as sr
-
-def getVoice():
-    r=sr.Recognizer()
-    with sr.Microphone() as source:
-        print("Say Something..!")
-        audio = r.listen(source)
-        print("done !\n")
-    try:
-        text = r.recognize_google(audio)
-        print("you said"+ text)
-    except Exception as e:
-        print(e)
-
-getVoice();
+data = input("Enter the text that you want to convert into speech:\n")
+engine = pyttsx3.init()
+engine.say(data)
+engine.runAndWait()
